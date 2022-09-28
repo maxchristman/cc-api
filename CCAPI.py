acb84e9738118dcfac83c1e2f49909e2ad55fcf9
Max Christman <54341372+maxchristman@users.noreply.github.com>
2022-09-28 16:31:35 -0400

    Added basic advisor lookup

--- CCAPI.py
+++ CCAPI.py
@@ -1,11 +1,16 @@
 import time, tomli
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class CCAPI:
-    def __login(self):
+    def __init__(self):
+        self.timeout = 10
 
-        driver = webdriver.Firefox()
+    def __login(self, driver):
+
         driver.set_page_load_timeout(10)
 
         driver.get("https://connectcarolina.unc.edu/")
@@ -18,7 +23,7 @@ class CCAPI:
             credentials = tomli.load(f)["credentials"]
         username, password = credentials["username"], credentials["password"]
 
-        _ = driver.implicitly_wait(10)
+        _ = driver.implicitly_wait(self.timeout)
 
         sso_form = driver.find_element(By.CLASS_NAME, "sso-form")
         username_input = driver.find_element(By.ID, "username")
@@ -28,5 +33,25 @@ class CCAPI:
         username_input.send_keys(username)
         password_input.send_keys(password)
         login_button.click()
+
+    def get_advisor(self):
+        driver = webdriver.Chrome()
+        self.__login(driver)
+
+        try:
+            advisor_element = WebDriverWait(driver, self.timeout).until(EC.presence_of_element_located((By.ID, "NC_CS_enr_tile_boxmiddleright")))
+        except TimeoutException:
+            print("Timeout loading CC.")
+
+
+        advisor = advisor_element.find_element(By.TAG_NAME, "a")
+        advisor_name = advisor.get_attribute("innerHTML").strip()
+        advisor_email = advisor.get_attribute("href")[7:].strip().lower()
+
+        advisor_info = {
+            "name": advisor_name,
+            "email": advisor_email
+        }
+
+        return advisor_info
         
-        return driver
---
+++ test.py
@@ -0,0 +1,4 @@
+from CCAPI import CCAPI
+
+ccapi = CCAPI()
+print(ccapi.get_advisor())
